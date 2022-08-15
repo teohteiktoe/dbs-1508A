8924214bf1b76932aa69813417bfe5d1ef5a5db9
Teoh TeikToe <ttt@mensa.org.sg>
2022-08-15 14:36:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 from flask import Flask, render_template, request
-
-
-# In[2]:
-
-
 app = Flask(__name__)
-
-
-# In[3]:
-
-
 import joblib
 
 @app.route("/", methods=["GET","POST"])
@@ -30,19 +14,8 @@ def index():
         return(render_template("index.html",result1=r1,result2=r2))
     else:
         return(render_template("index.html",result1="waiting",result2="waiting"))
-
-
-# In[ ]:
-
-
 if __name__ == "__main__":
     app.run()
-
-
-# In[ ]:
-
-# In[ ]:
-
 
 
 
